67365e85a520746b81563a7750d1b1b868861422
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-07-30 22:33:41 -0400

    added forgotten routes

--- routes/thought_routes.js
+++ routes/thought_routes.js
@@ -53,6 +53,53 @@ router.post('/api/thoughts', async (req, res) => {
   }
 });
 
-
+//  Update a Thought
+router.put('/api/thoughts/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { title, content } = req.body;
+  
+      const thought = await Thought.findById(id);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+  
+      thought.title = title;
+      thought.content = content;
+      await thought.save();
+  
+      res.status(200).json(thought);
+    } catch (error) {
+      console.error('Error updating thought:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  // Remove a Thought by ID
+  router.delete('/api/thoughts/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+  
+      const thought = await Thought.findById(id);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+  
+      // Remove thought from a User
+      const userId = thought.userId; // Assuming you have a userId field in the Thought model
+      const user = await User.findById(userId);
+      if (user) {
+        user.thoughts.pull(id);
+        await user.save();
+      }
+  
+      await thought.remove();
+  
+      res.status(200).json({ message: 'Thought Removed' });
+    } catch (error) {
+      console.error('Error removing thought:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
 
 module.exports = router;
